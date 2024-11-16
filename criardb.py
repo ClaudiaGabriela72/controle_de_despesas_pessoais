7600513da5b9fa8ba09e9ec0197b77a695b3ee6d
ClaudiaGabriela72 <71900311+ClaudiaGabriela72@users.noreply.github.com>
2024-11-16 07:56:59 -0400

    criandodb

---
+++ criardb.py
@@ -0,0 +1,21 @@
+import sqlite3 as lite
+
+# criando conexao
+con = lite.connect('dados.db')
+
+ # criando tabela de categoria
+with con:
+    cur = con.cursor()
+    cur.execute('CREATE TABLE Categoria(id INTEGER PRIMARY KEY AUTOINCREMENT,nome TEXT )')
+
+
+ # criando tabela de receitas
+with con:
+    cur = con.cursor()
+    cur.execute('CREATE TABLE Receitas(id INTEGER PRIMARY KEY AUTOINCREMENT,categoria TEXT, adicionando_em DATE, adicionado_em DATE, valor DECIMAl)')
+
+
+ # criando tabela de gastos
+with con:
+    cur = con.cursor()
+    cur.execute('CREATE TABLE Gastos(id INTEGER PRIMARY KEY AUTOINCREMENT,categoria TEXT, adicionando_em DATE, retirado_em DATE, valor DECIMAl)')
