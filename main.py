0e5f57911279ea380ed005c88dc4f4bac04d3642
ClaudiaGabriela72 <71900311+ClaudiaGabriela72@users.noreply.github.com>
2024-11-18 20:31:15 -0400

    at

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from tkinter import *
-from tkinter import Tk, ttk
-import PIL
+from tkinter import Tk, ttk, Label
+from PIL import Image, ImageTk
 import PIL.Image
+
+
+from tkinter.ttk import Progressbar
+
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+import matplotlib.pyplot as plt
+from matplotlib.figure import Figure
+
 
 
 # cores
@@ -39,22 +47,78 @@ frameBaixo  = Frame(janela, width=1043, height=300, bg=co1, relief='flat')
 frameBaixo.grid(row=2, column=0,pady=0, padx=10, sticky=NSEW)
 
 # Frame cima
-
 global app_img
-app_img = Image("icons8-usuário-do-bloco-de-notas-50.png")
-app_img = app_img.resize((45,45))
-app_img = Tk.PhotoImage(app_img)
 
+img = Image.open("logo.png")
+app_img = ImageTk.PhotoImage(img)
 
 
 app_logo = Label(frameCima, image=app_img, text="Orçamento pessoal", width=900, compound=LEFT, padx=5, relief=RAISED, anchor=NW, font=('Verdana 20 bold'), bg=co1, fg=co4)
 app_logo.place(x=7, y=5)
 
-janela.mainloop()
+# Porcentagem
 
-#root = tk.Tk()
-#photo = ImageTk.PhotoImage(image)
-#label = tk.Label(root, image=photo)
-#label.image = image
-#label.pack()
-#photo = ImageTk.PhotoImage(image, master=root)
+def percentagem():
+    l_nome = Label(frameMeio, text="Porcentagem da receita gasta", height=1, anchor=NW, font=('Verdana 12'), bg=co1, fg=co4)
+    l_nome.place(x=7, y=5)
+
+    style = ttk.Style()
+    style.theme_use('default')
+    style.configure('back.Horizontal.TProgressbar', backgroung='#daed6b')
+    style.configure('TProgressbar', thickness=20)
+
+    bar = Progressbar(frameMeio, length=180, style='black.Horizontal.TProgressbar')
+    bar.place(x=10, y=35)
+    bar['value'] = 50
+
+    valor = 50
+
+    l_percentagem = Label(frameMeio, text="{:,.2f}%".format(valor), anchor=NW, font=('Verdana 12'), bg=co1, fg=co4)
+    l_percentagem.place(x=200, y=35)
+
+
+# Funçao para grafico
+def grafico_bar():
+    lista_categorias = ['Renda', 'Despesas', 'Saldo']
+    lista_valores = [3000, 2000, 6236]
+
+
+    #fazendo figura e atribuindo objestos de eixo
+    figura = plt.Figure(figsize=(4, 3.45), dpi=60)
+    ax = figura.add_subplot(111)
+
+    ax.bar(lista_categorias, lista_valores, color=colors, width=0.9)
+
+    c = 0
+
+    for i in ax.patches:
+        ax.text(i.get_x()-.001, i.get_height()+.5,
+        str("{:,.0f}".format(lista_valores[c])), fontsize=17, fontstyle='italic', verticalalignment='bottom',)
+        c += 1
+    
+    ax.set_xticklabels(lista_categorias, fontsize=16)
+
+    ax.patch.set_facecolor('#ffffff')
+    ax.spines['bottom'].set_color('#cccccc')
+    ax.spines['bottom'].set_linewidth(1)
+    ax.spines['right'].set_linewidth(0)
+    ax.spines['top'].set_linewidth(0)
+    ax.spines['left'].set_color('#cccccc')
+    ax.spines['left'].set_linewidth(1)
+
+
+    ax.spines['top'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+    ax.spines['left'].set_visible(False)
+    ax.tick_params(bottom=False, left=False)
+    ax.set_axisbelow(True)
+    ax.yaxis.grid(False, color='#eeeeee')
+    ax.xaxis.grid(False)
+
+    canva = FigureCanvasTkAgg(figura, frameMeio)
+    canva.get_tk_widget().place(x=10, y=70)
+
+
+percentagem()   
+grafico_bar()
+janela.mainloop()
